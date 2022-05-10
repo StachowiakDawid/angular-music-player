5a137039bcfbf4dbded891f3738c4c5b67f8c2bf
dawidos234 <dawidos234@gmail.com>
2022-05-10 12:59:42 +0200

    Initial commit

---
+++ client/src/app/search-history/search-history.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SearchHistoryService } from '../search-history.service';
+import { SearchService } from '../search.service';
+
+@Component({
+    selector: 'app-search-history',
+    templateUrl: './search-history.component.html',
+    standalone: false
+})
+
+export class SearchHistoryComponent implements OnInit {
+  searchHistory: Array<string> = this.searchHistoryService.getSearchHistory();
+  inputPhrase!: string;
+  constructor(private searchHistoryService: SearchHistoryService, private searchService: SearchService) { }
+
+  ngOnInit(): void {
+    this.searchService.searchPhrase.subscribe((data) => this.inputPhrase = data);
+  }
+
+  deleteItem (index: number): void {
+    this.searchHistoryService.removeItem(index);
+    this.searchHistory = this.searchHistoryService.getSearchHistory();
+  }
+
+}
